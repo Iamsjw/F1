d9fa45151a7237cf78731292f3bb1fe33067e839
Nova <131579958+Iamsjw@users.noreply.github.com>
2026-06-27 21:39:53 +0530

    Update student_enrollment_tab.dart

--- lib/presentation/admin_dashboard_screen/widgets/student_enrollment_tab.dart
+++ lib/presentation/admin_dashboard_screen/widgets/student_enrollment_tab.dart
@@ -402,7 +402,7 @@ class _StudentEnrollmentTabState extends State<StudentEnrollmentTab> {
 
   Future<void> _importStudentsFromCsv() async {
     try {
-      final result = await FilePicker.platform.pickFiles(
+      final result = await FilePicker.instance.pickFiles(
         type: FileType.custom,
         allowedExtensions: ['csv'],
       );
